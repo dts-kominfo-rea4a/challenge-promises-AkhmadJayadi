11f79befa9eecd045c024188fa374aaf28ed6db7
Akhmad Jayadi <jayadiakhmad17@gmail.com>
2022-09-19 15:43:20 +0700

    tugas2

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (expresion) => {
+  let count = 0;
+  try {
+    const tiix = await promiseTheaterIXX();
+    const tvgc = await promiseTheaterVGC();
+    const result = [...tiix, ...tvgc];
+    
+    result.forEach((Element) => {
+      if (Element.hasil == expresion) {
+        count++;
+      }
+    });
+    if (count > 0) {
+      return count;
+    };
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 module.exports = {
   promiseOutput,
